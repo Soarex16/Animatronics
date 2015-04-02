7297989dad4bd0e9e8cf455e6d3afbdaddce80f0
Animatonics <shumik1337@gmail.com>
2015-04-02 23:35:39 +0400

    Remover unused hadlers (cloud of crutches *~*)

--- java/animatronica/debug/TileEntityDebug.java
+++ java/animatronica/debug/TileEntityDebug.java
@@ -15,12 +15,15 @@ public class TileEntityDebug extends TileAnimatronica {
 	@Override
 	public void updateEntity() {
 		rotate+=0.9;
-
+		
+		Animatronica.proxy.sparkleFX(worldObj, xCoord+0.5F, yCoord + 1.0F, zCoord + 0.5F, (float)Math.random(), (float)Math.random(), (float)Math.random(), (float)(Math.random()*1.5F), 50);
+/*
 		Animatronica.proxy.setWispFXDistanceLimit(false);
-		Animatronica.proxy.wispFX(worldObj, xCoord + 0.5, yCoord + 0.5, zCoord + 0.5, 0.075F, 0.75F, 0.075F, (float) Math.random() * 5 + 1F, (float) (Math.random() - 0.5F), 10F * (float) Math.sqrt(10/(30F + yCoord)), (float) (Math.random() - 0.5F));
+		Animatronica.proxy.wispFX(worldObj, xCoord + 0.5, yCoord + 0.5, zCoord + 0.5, 0.075F, 0.75F, 0.075F, (float) Math.random() * 4 + 1F, (float) (Math.random() - 0.5F), 5F * (float) Math.sqrt(10/(30F + yCoord)), (float) (Math.random() - 0.5F));
 
-		for(int i = 0; i < 2; i++)
-			Animatronica.proxy.wispFX(worldObj, xCoord + 0.5, yCoord + 75, zCoord + 0.5, 0.075F, 0.75F, 0.075F, (float) Math.random() * 15 + 4F, (float) (Math.random() - 0.5F) * 4F, 0F, (float) (Math.random() - 0.5F) * 4F);
+		for(int i = 0; i < 1; i++)
+			Animatronica.proxy.wispFX(worldObj, xCoord + 0.5, yCoord + 75, zCoord + 0.5, 0.075F, 0.75F, 0.075F, (float) Math.random() * 7 + 4F, (float) (Math.random() - 0.5F) * 3F, 0F, (float) (Math.random() - 0.5F) * 3F);
 		Animatronica.proxy.setWispFXDistanceLimit(true);
+*/
 	}
 }
--- java/animatronica/utils/handler/IParticleHandler.java
+++
@@ -1,9 +0,0 @@
-package animatronica.utils.handler;
-
-import net.minecraft.world.World;
-
-public interface IParticleHandler {
-	
-	public void sparkleFX(World world, double x, double y, double z, float r, float g, float b, float size, int m);
-
-}
