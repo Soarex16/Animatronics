7c79a48097ef48e6ece0854910a61a115def9168
B10nicMachine <shumik1337@gmail.com>
2015-08-26 00:07:09 +0300

    GuiConfig sorted by categories

--- java/animatronics/utils/config/AnimatronicsConfigGui.java
+++ java/animatronics/utils/config/AnimatronicsConfigGui.java
@@ -1,14 +1,24 @@
 package animatronics.utils.config;
 
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+import animatronics.Animatronics;
+import cpw.mods.fml.client.config.GuiConfig;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.common.config.ConfigElement;
-import net.minecraftforge.common.config.Configuration;
-import cpw.mods.fml.client.config.GuiConfig;
 
 public class AnimatronicsConfigGui extends GuiConfig{
     
 	public AnimatronicsConfigGui(GuiScreen parent) {
-        super(parent, new ConfigElement(AnimatronicsConfiguration.configFile.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(), "Animatronics", false, false, "Animatronics");
-    }
+		super(parent, getConfigElements(), Animatronics.MOD_ID, false, false, "Animatronics");
+	}
+	
+	private static List getConfigElements() {
+		List<ConfigElement> list = Lists.newArrayList();
+		list.add(new ConfigElement(AnimatronicsConfiguration.configFile.getCategory("energy")));
+		return list;
+	}
 }
  
--- java/animatronics/utils/config/AnimatronicsConfiguration.java
+++ java/animatronics/utils/config/AnimatronicsConfiguration.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 import animatronics.Animatronics;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
 
 public class AnimatronicsConfiguration{
 
@@ -13,7 +14,7 @@ public class AnimatronicsConfiguration{
     public static float generatorMaxEntropy = 1000000;
     public static float consumerMaxEntropy = 100000;
     public static float maxEnergyDistance = 16;
-
+    
 	public static void init(String cfgPath){
 		
 
@@ -38,11 +39,11 @@ public class AnimatronicsConfiguration{
 	
 	public static void syncConfig(){
 		
-		maxEntropy = configFile.getInt("MAX Entropy for Machines", Configuration.CATEGORY_GENERAL, maxEntropy, 1000, 100000000, "This variable used to set maximum entropy in machines");
-		generatorMaxEntropy = configFile.getFloat("Generator MAX Entropy", Configuration.CATEGORY_GENERAL, generatorMaxEntropy, 1000, 100000000, "This variable used to set maximum entropy capacity in generators");
-		consumerMaxEntropy = configFile.getFloat("Consumer MAX Entropy", Configuration.CATEGORY_GENERAL, consumerMaxEntropy, 1000, 100000000, "This variable used to set maximum entropy capacity in consumers");
-		maxEnergyDistance = configFile.getFloat("MAX Entropy Transfering distance", Configuration.CATEGORY_GENERAL, maxEnergyDistance, 2, 512, "This variable used to set distance of entropy transfering");
-		
+		maxEntropy = configFile.getInt("MAX Entropy for Machines", "energy", maxEntropy, 1000, 100000000, "This variable used to set maximum entropy in machines");
+		generatorMaxEntropy = configFile.getFloat("Generator MAX Entropy", "energy", generatorMaxEntropy, 1000, 100000000, "This variable used to set maximum entropy capacity in generators");
+		consumerMaxEntropy = configFile.getFloat("Consumer MAX Entropy", "energy", consumerMaxEntropy, 1000, 100000000, "This variable used to set maximum entropy capacity in consumers");
+		maxEnergyDistance = configFile.getFloat("MAX Entropy Transfering distance", "energy", maxEnergyDistance, 2, 512, "This variable used to set distance of entropy transfering");
+				
 		if(configFile.hasChanged()){
 			configFile.save();
 		}
