ff9ca68b5c16e9d42b4cba02b65d7229713c1df4
B10nicMachine <shumik1337@gmail.com>
2015-09-16 19:06:19 +0300

    code clean

--- java/animatronics/client/render/tile/RenderTileEntityFabricatorOfEverything.java
+++ java/animatronics/client/render/tile/RenderTileEntityFabricatorOfEverything.java
@@ -4,10 +4,11 @@ import java.awt.Color;
 
 import org.lwjgl.opengl.GL11;
 
+import animatronics.api.misc.Vector3;
+import animatronics.client.render.BlockOutlineRender;
 import animatronics.client.render.RenderPatterns;
 import animatronics.utils.handler.ClientTickHandler;
 import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
@@ -16,6 +17,6 @@ public class RenderTileEntityFabricatorOfEverything extends TileEntitySpecialRen
 
 	@Override
 	public void renderTileEntityAt(TileEntity tile, double x, double y, double z, float f) {
-		//block outline render here...It din't want to render
+		
 	}
 }
